b4ec9bc480ffde0de2d6dcf4456a82cbac257356
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-04-24 03:15:56 +0900

    enable inference of output filename

--- imio.py
+++ imio.py
@@ -4,8 +4,10 @@ import argparse
 import numpy as np
 from PIL import Image
 
-def txtname(filename):
-    return re.sub(r'(.*)\.(.*)?', r'\1.txt', filename)
+def suffix(filename, suffix):
+    if not suffix.startswith('.'):
+        suffix = '.' + suffix
+    return re.sub(r'(.*)\.(.*)?', r'\1' + suffix, filename)
 
 def upper_bound_to_scale(input_size:tuple, upper_bound:int):
     longer_edge = max(input_size)
@@ -15,7 +17,7 @@ def upper_bound_to_scale(input_size:tuple, upper_bound:int):
 def rescale(image, scale):
     return image.resize((np.asarray(image.size) * scale).astype(int))
 
-def image_to_txt(input_fname, output_fname, size=None, scale=None, upper_bound=None):
+def image_to_txt(input_fname, output_fname=None, size=None, scale=None, upper_bound=None):
     with Image.open(input_fname) as image:
         if upper_bound:
             scale = upper_bound_to_scale(image.size, upper_bound)
@@ -26,7 +28,8 @@ def image_to_txt(input_fname, output_fname, size=None, scale=None, upper_bound=N
             image = rescale(image, scale)
             
         array = np.asarray(image.convert('L'))
-        output_fname = txtname(output_fname)
+        if output_fname is None:
+            output_fname = suffix(input_fname, '.txt')
         np.savetxt(output_fname, array, fmt='%d', newline=' ')
 
         with open(output_fname, 'r+') as f:
@@ -35,9 +38,11 @@ def image_to_txt(input_fname, output_fname, size=None, scale=None, upper_bound=N
             f.write(f"{image.size[0]} {image.size[1]}\n" + content)
         
 
-def txt_to_image(input_fname, output_fname):
-    array = np.loadtxt(txtname(input_fname))
+def txt_to_image(input_fname, output_fname=None):
+    array = np.loadtxt(input_fname)
     image = Image.fromarray(array).convert('RGB')
+    if output_fname is None:
+        output_fname = suffix(input_fname, '.jpg')
     image.save(output_fname)
     
 if __name__ == '__main__':
@@ -45,7 +50,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Image I/O')
     parser.add_argument('subcommand')
     parser.add_argument('input_filename')
-    parser.add_argument('output_filename')
+    parser.add_argument('-o', dest='output_filename')
     parser.add_argument('--size', type=int, nargs=2)
     parser.add_argument('--scale', type=float)
     parser.add_argument('--upper-bound', type=int)
