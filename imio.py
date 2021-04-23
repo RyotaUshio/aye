cb71257bb6bde8148627643eefd73daadb4c9208
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-04-23 21:26:52 +0900

    add images

---
+++ imio.py
@@ -0,0 +1,57 @@
+import re
+import sys
+import argparse
+import numpy as np
+from PIL import Image
+
+def txtname(filename):
+    return re.sub(r'(.*)\.(.*)?', r'\1.txt', filename)
+
+def upper_bound_to_scale(input_size:tuple, upper_bound:int):
+    longer_edge = max(input_size)
+    scale = upper_bound / longer_edge
+    return scale
+
+def rescale(image, scale):
+    return image.resize((np.asarray(image.size) * scale).astype(int))
+
+def image_to_txt(input_fname, output_fname, size=None, scale=None, upper_bound=None):
+    with Image.open(input_fname) as image:
+        if upper_bound:
+            scale = upper_bound_to_scale(image.size, upper_bound)
+            image = rescale(image, scale)
+        elif size:
+            image = image.resize(size)
+        elif scale:
+            image = rescale(image, scale)
+            
+        array = np.asarray(image.convert('L'))
+        output_fname = txtname(output_fname)
+        np.savetxt(output_fname, array, fmt='%d', newline=' ')
+
+        with open(output_fname, 'r+') as f:
+            content = f.read()
+            f.seek(0)
+            f.write(f"{image.size[0]} {image.size[1]}\n" + content)
+        
+
+def txt_to_image(input_fname, output_fname):
+    array = np.loadtxt(txtname(input_fname))
+    image = Image.fromarray(array).convert('RGB')
+    image.save(output_fname)
+    
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Image I/O')
+    parser.add_argument('subcommand')
+    parser.add_argument('input_filename')
+    parser.add_argument('output_filename')
+    parser.add_argument('--size', type=int, nargs=2)
+    parser.add_argument('--scale', type=float)
+    parser.add_argument('--upper-bound', type=int)
+    args = parser.parse_args()
+    
+    if args.subcommand == 'to_txt':
+        image_to_txt(args.input_filename, args.output_filename, size=args.size, scale=args.scale, upper_bound=args.upper_bound)
+    elif args.subcommand == 'to_image':
+        txt_to_image(args.input_filename, args.output_filename)
--- vision/io.cpp
+++
@@ -1,6 +0,0 @@
-#include <io.hpp>
-
-np::ndarray<double> vision::imread(const std::string& filename) {
-  std::string python_script =
-    "from PIL import 
-}
