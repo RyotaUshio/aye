91c38df26552951031a5d4097a64350418ff4d61
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-04-25 04:16:37 +0900

    minot change

--- eyeball/core.hpp
+++ eyeball/core.hpp
@@ -3,8 +3,10 @@
 #include <numpy/numpy.hpp>
 
 namespace eyeball {
-  using Image = numpy::ndarray<double>;
-  using Kernel = numpy::ndarray<double>;
+  using Image = numpy::ndarray<numpy::float_>;
+  using Kernel = numpy::ndarray<numpy::float_>;
+  using CImage = numpy::ndarray<numpy::complex_>;
+  using CKernel = numpy::ndarray<numpy::complex_>;
 }
 
 namespace np = numpy;
--- test/test_harris.cpp
+++ test/test_harris.cpp
@@ -4,7 +4,7 @@ namespace np = numpy;
 
 int main() {
   try {
-    auto image = eyeball::sample::corner;
+    auto image = eyeball::sample::checkerboard;
     auto harris = eyeball::Harris(image, eyeball::Gauss(1), 3);
     eyeball::savetxt("./images/harris_response.txt", harris.response);
   } catch(const std::exception& e) {
